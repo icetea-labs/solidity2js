2b0327927e54a62e0caa4bf3f676a49343d0b79d
Tung Duong <tungduonghgg123@gmail.com>
2019-03-31 23:20:06 +0700

    generate require method in class

--- src/compiler/Visitors/contract.js
+++ src/compiler/Visitors/contract.js
@@ -1,4 +1,5 @@
-import { classDeclaration, classBody, identifier } from "@babel/types";
+import { classDeclaration, classBody, identifier, classMethod, blockStatement, 
+        ifStatement, newExpression, throwStatement} from "@babel/types";
 export default {
     //add @contract 
     ContractDefinition: function(node, parent) {
@@ -9,6 +10,27 @@ export default {
         )
         node._context = classNode.body.body	;
         parent._context.push(classNode)
+    },
+    //add require function 
+    'ContractDefinition:exit': function (node, parent) {
+        var body = blockStatement([
+            ifStatement(
+                identifier('!condition'),
+                blockStatement([
+                    throwStatement(newExpression(
+                        identifier('Error'),
+                        [identifier('errorMessage')]
+                    ))
+                ]),
+            )
+        ]);
+        var requireMethodNode = classMethod(
+                'method', 
+                identifier('require'),
+                [identifier('condition'),identifier('errorMessage')],
+                body
+        );
+        node._context.push(requireMethodNode);
     },
     PragmaDirective: function(node, parent) {
         const commentNode = {
